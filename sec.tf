ead475d7be1abd679f632f495b2470e6763422ae
ihoslav <71399919+ihoslav@users.noreply.github.com>
2021-08-03 17:01:02 -0500

    Update sec.tf

--- sec.tf
+++ sec.tf
@@ -22,6 +22,15 @@ ingress {
 
   }
 
+  ingress {
+    description      = "for jenkins"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+  
   egress {
     from_port        = 0
     to_port          = 0
