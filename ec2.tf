ac5288e932dd1eaf07868b643f86326e59397c18
ihoslav <71399919+ihoslav@users.noreply.github.com>
2021-08-03 16:20:52 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "ihos-ec3" {
   instance_type = var.instance-type
   security_groups = [ aws_security_group.ihos-sec.name ]
   key_name = aws_key_pair.virgin-key.key_name
-  user_data = file("user-data-updateEC2.sh")
+  user_data = file("user-data-updateEC2")
 
   tags = {
     Name = "virgin-ec3"
