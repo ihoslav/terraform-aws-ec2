f0431425c76636d60e8b45bbf85c89cf4ddb22c4
ihoslav <71399919+ihoslav@users.noreply.github.com>
2021-08-19 17:40:41 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "ihos-ec3" {
   ami = var.ami
   instance_type = var.instance-type
   security_groups = [ aws_security_group.ihos-sec.name ]
-  key_name = aws_key_pair.virgin-key.key_name
+  key_name = "bastion-key"
   user_data = file("user-data-updateEC2")
 
   tags = {
